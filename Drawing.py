b28c7cca2a306b2c585d3f205247933089d49046
KrzysztofSkos <krzysztof.skos@gmail.com>
2021-05-11 23:28:28 +0200

    Added 3d plot

---
+++ Drawing.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on May 11 22:42:06 2021
+@author: krzysztof_skos
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
+
+
+def __init__():
+    fig = plt.figure()
+
+
+def data_for_cylinder_along_z(center_x, center_y, radius, height_z):
+    z = np.linspace(0, height_z, 50)
+    theta = np.linspace(0, 2 * np.pi, 50)
+    theta_grid, z_grid = np.meshgrid(theta, z)
+    x_grid = radius * np.cos(theta_grid) + center_x
+    y_grid = radius * np.sin(theta_grid) + center_y
+    return z_grid, y_grid, x_grid
+
+
+def drawPlot(rad, ax):
+    ax.clear()
+    ax.scatter(25, 0, rad)
+    ax.text(26, 0, rad, "router")
+
+    Xc, Yc, Zc = data_for_cylinder_along_z(0, 0, rad, 50)
+    ax.plot_surface(Xc, Yc, Zc, alpha=0.5)
+
+    ax.set_xlabel("X")
+    ax.set_ylabel("Y")
+    ax.set_zlabel("Z")
+    plt.show()
--- Simulator.py
+++ Simulator.py
@@ -5,7 +5,18 @@ Created on May 03 20:55:07 2021
 @author: krzysztof_skos
 """
 
+import time
+import matplotlib.pyplot as plt
 from NanoNode import NanoNode
+import Drawing
 
-node = NanoNode(10, [10, 10, 10], 10, 1)
-node.printData()
+# node = NanoNode(10, [10, 10, 10], 10, 1)
+# node.printData()
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+for i in range(3, 10):
+    print(i)
+    Drawing.drawPlot(i, ax)
+    time.sleep(1)
